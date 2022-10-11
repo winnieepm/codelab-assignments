7a9131bcd37def10ee61a49a2d403b1f788b3f97
donttrust <71612467+winnieepm@users.noreply.github.com>
2022-10-11 15:41:09 -0400

    streamlined answer

--- wk5_fizzbuzz.py
+++ wk5_fizzbuzz.py
@@ -1,13 +1,12 @@
 ints = int(input('Write a number: '))
 
 def fizbuzz(num):
-    if num%3==0:
-        if num%5==0:
-                print('fizzbuzz')
-        else:
-                print('fizz')
+    if num%3==0 and num%5==0:
+        print('fizzbuzz')
     elif num%5==0:
             print('buzz')
+    elif num%3==0:
+        print('fizz')
     else:
             print(num)
 
