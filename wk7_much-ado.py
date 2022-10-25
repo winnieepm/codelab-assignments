efdeba561dc007d0c98fe0e428f9bb41515081b0
donttrust <71612467+winnieepm@users.noreply.github.com>
2022-10-24 23:16:11 -0400

    finally done

--- wk7_much-ado.py
+++ wk7_much-ado.py
@@ -1,38 +1,37 @@
 # INSTRUCTIONS
 # Write a program to read in this file and count the total lines of dialog and the length (character count) of those lines for the two main characters: Benedick and Beatrice. Write that data out to a second file.
 
-
-
-# STEP 0 turn file content into a line list called lines
-lines=[]
+# upload file
 with open('wk7.txt', 'r') as infile:
-    for line in infile:
-        lines.append(line)
+    text = infile.read()
 
-# print(lines)
-# STEP 1 find target person's name in lines list
-ben=0
-ben_char=0
 bea=0
+ben=0
 bea_char=0
-for index, line in enumerate(lines):
-    if line=="BENEDICK.\n":
+ben_char=0
+lines= text.split("\n\n")
+
+for line in lines:
+    line.replace('\n', ' ')
+
+    if line.startswith('BEATRICE.'):
+        bea+=1
+        bea_char+=len(line)-9
+        
+    elif line.startswith('BENEDICK.'):
         ben+=1
-        while line!='\n':
-            print(len(line))
-        nextLine=lines[index+1]
-
-    # if line=="BEATRICE.\n":
-    #     bea+=1
-
-# print(ben)
-# print(bea)
+        ben_char+=len(line)-9
 
 
-# STEP 2 find all words BETWEEN chosen person and the next character speaking
-# 2.1 define stop parameter
+# print("Beatrice has " + str(bea) + " lines which equals " + str(bea_char) + " characters")
+# print("Benedick has " + str(ben) + " lines which equals " + str(ben_char) + " characters")
 
 
 
 
-# 2.2 add dialogue lines to ben=[] or bea=[]
+
+# STEP 3 printout
+with open('wk7_result.txt', 'w') as outfile:
+    outfile.write('Beatrice has '+str(bea)+' lines which equals '+str(bea_char)+' characters.\n')
+    outfile.write('Benedick has '+str(ben)+' lines which equals '+str(ben_char)+' characters.\n')
+    outfile.close()
