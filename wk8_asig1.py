dcc7d07f9f49e09a4863f7ad8fc807047692e531
donttrust <71612467+winnieepm@users.noreply.github.com>
2022-11-02 01:56:45 -0400

    generated + structured data

---
+++ wk8_asig1.py
@@ -0,0 +1,21 @@
+# Think about everything you know about your Praxis cohort (the icebreakers doc is fair game). Think about the kind of questions that might interest you about such a corpus and the best way to structure that information to answer those questions. Write some Python code in the vein of the Scholars Lab example in the data structures review that represents the model you come up with. This one is intentionally a bit amorphous, but any practice in thinking about the shape of data is useful.
+
+# data
+caroline = {'bio': {'born':'united states', 'edu':'phd student', 'job_exp':'teaching asistant', 'dept':'art history'}, 'icebreak': {'best_tacos': 'brazos tacos', 'bad_movie':'tremors'}}
+
+malcolm = {'bio': {'born':'united states', 'edu':'phd student', 'job_exp':'teaching asistant', 'dept':'history'}, 'icebreak': {'best_tacos': 'brazos tacos', 'bad_movie':'anything kevin heart', 'worst_dish':'iceberg lettuce'}}
+
+samantha = {'bio': {'born':'jamaica', 'edu':'phd student', 'job_exp':['instructor', 'research assistant'], 'dept':'english'}, 'icebreak': {'best_tacos': 'don\'t know', 'bad_movie':'a knight\'s tale (2001)', 'worst_dish':'cornmeal porridge'}}
+
+winnie = {'bio': {'born':'puerto rico', 'edu':'phd student', 'job_exp':['instructor', 'research assistant'], 'dept':'spanish'}, 'icebreak': {'best_tacos': 'don\'t know', 'bad_movie':['labyrinth (1986)', 'waiting (2005)'], 'worst_dish':'cornmeal porridge'}}
+
+
+# ask questions
+#1- Are the movies chosen for favorite bad movie similar decade?
+
+#2- What are the worst dishes they've eaten?
+
+#3- Is this cohort a diverse group?
+
+
+# FINAL STEP: print output from each question into a txt file. 
